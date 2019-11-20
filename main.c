415c54fca46ebe2286406cbbad7ec9edcb07d44d
barti <barteksredzinski@gmail.com>
2019-11-20 23:32:12 +0100

    added loading and drawing map

--- main.c
+++ main.c
@@ -3,10 +3,75 @@
 
 const gchar* path;
 double cycle = 0.2;
+int width = 120, height = 80;
+static char** map = NULL;
+
+static void checkConditions(GtkWidget*, GtkWidget*);
+static void getPath_callback(GtkWidget*, GtkFileChooser*);
+static void on_changed(GtkComboBox*, gpointer);
+static gboolean on_draw_event(GtkWidget*, cairo_t*, gpointer);
+static void activate(GtkApplication*, gpointer);
+static void readMap(FILE*);
+void freeMap();
+
+static void initMap(){
+    if(map == NULL){
+        map = malloc(sizeof(char*) * width);
+        for(int i = 0; i < width; i++){
+            map[i] = malloc(sizeof(char) * height);
+        }
+    }
+
+    for(int i = 0; i < width; i++){
+        for(int j = 0; j < height; j++){
+            map[i][j] = '.';
+        }
+    }
+}
+
+static void readMap(FILE* file){
+    fseek (file, 0, SEEK_END);
+    int size = ftell (file);
+    fseek (file, 0, SEEK_SET);
+
+    char text[255];
+    fgets(text, 255, file);
+
+    int fileWidth = strlen(text);
+    int fileHeight = size / fileWidth;
+
+    fseek (file, 0, SEEK_SET);
+    int yoffset = (height - fileHeight) / 2;
+    int xoffset = (width - fileWidth) / 2;
+
+    while(fgets(text, 255, file) != NULL) {
+        for (int i = 0; i < fileWidth - 1; i++) {
+            map[yoffset][xoffset + i] = text[i];
+        }
+        yoffset++;
+    }
+}
 
 static void checkConditions(GtkWidget *widget, GtkWidget *button){
     if(path != NULL && cycle != 0){
-        g_print("path: %s, time: %f", path, cycle);
+        FILE *file = fopen(path, "r");
+        if(!file==NULL){
+            initMap();
+            readMap(file);
+        }
+        else{
+            g_print("Error: reading new file");
+        }
+
+    }
+}
+
+void freeMap(){
+    if(map != NULL) {
+        for (int i = 0; i < width; i++) {
+            free(map[i]);
+        }
+        free(map);
     }
 }
 
@@ -14,7 +79,7 @@ static void getPath_callback(GtkWidget *widget, GtkFileChooser *chooser){
     path = gtk_file_chooser_get_filename(widget);
 }
 
-static void on_changed(GtkComboBox *widget, gpointer   user_data){
+static void on_changed(GtkComboBox *widget, gpointer user_data){
     GtkComboBox *combo_box = widget;
 
     if (gtk_combo_box_get_active (combo_box) != 0) {
@@ -22,59 +87,41 @@ static void on_changed(GtkComboBox *widget, gpointer   user_data){
     }
 }
 
-static void update_preview_cb(GtkFileChooser *file_chooser, gpointer data){
-    GtkWidget *preview;
-    char *filename;
-    GdkPixbuf *pixbuf;
-    gboolean have_preview;
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data){
+    GtkWidget *win = gtk_widget_get_toplevel(widget);
 
-    preview = GTK_WIDGET (data);
-    filename = gtk_file_chooser_get_preview_filename (file_chooser);
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    cairo_paint(cr);
+    if(map != NULL) {
+        for (int i = 0, x = 0; i < width; i++, x += 8) {
+            for (int j = 0, y = 0; j < height; j++, y += 8) {
+                if (map[i][j] == '#') {
+                    cairo_set_line_width(cr, 1);
+                    cairo_set_source_rgb(cr, 1.f, 0.f, 0.f);
 
-    pixbuf = gdk_pixbuf_new_from_file_at_size (filename, 128, 128, NULL);
-    have_preview = (pixbuf != NULL);
-    g_free (filename);
-
-    gtk_image_set_from_pixbuf (GTK_IMAGE (preview), pixbuf);
-    if (pixbuf)
-        g_object_unref (pixbuf);
-
-    gtk_file_chooser_set_preview_widget_active (file_chooser, have_preview);
-}
-
-gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data){
-    guint width, height;
-    GdkRGBA color;
-    GtkStyleContext *context;
-
-    context = gtk_widget_get_style_context (widget);
-
-    width = gtk_widget_get_allocated_width (widget);
-    height = gtk_widget_get_allocated_height (widget);
-
-    gtk_render_background (context, cr, 0, 0, width, height);
-    cairo_arc (cr, width / 2.0, height / 2.0, MIN (width, height) / 2.0, 0, 2 * G_PI);
-    gtk_style_context_get_color (context, gtk_style_context_get_state (context), &color);
-    gdk_cairo_set_source_rgba (cr, &color);
-
-    cairo_fill (cr);
-
+                    cairo_rectangle(cr, y, x, 8, 8);
+                    cairo_stroke_preserve(cr);
+                    cairo_set_source_rgb(cr, 0.f, 1.f, 0.f);
+                    cairo_fill(cr);
+                }
+            }
+        }
+    }
     return FALSE;
 }
 
 static void activate(GtkApplication* app, gpointer user_data){
     GtkWidget *window;
     GtkWidget *vbox, *hbox1;
-    GtkWidget *choosebutton;
-    GtkWidget *button1;
-    GtkWidget *combo_box;
-    GtkWidget *drawing_area;
+    GtkWidget *chooseButton;
+    GtkWidget *startButton;
+    GtkWidget *comboBox;
+    GtkWidget *drawingArea;
 
 
     window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "Game of life");
-    gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
-
+    gtk_window_set_default_size (GTK_WINDOW (window), 960, 700);
 
     vbox = gtk_vbox_new(FALSE, 0);
     gtk_container_add(GTK_CONTAINER(window), vbox);
@@ -82,31 +129,29 @@ static void activate(GtkApplication* app, gpointer user_data){
 
     hbox1 = gtk_hbox_new(FALSE, 0);
     gtk_widget_show(hbox1);
-
     gtk_box_pack_start(GTK_BOX(vbox), hbox1, FALSE, FALSE, 5);
 
-    choosebutton = gtk_file_chooser_button_new (("Select a file"), GTK_FILE_CHOOSER_ACTION_OPEN);
-    gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (choosebutton), "/home/barti/CLionProjects");
-    g_signal_connect(choosebutton, "selection-changed", G_CALLBACK(getPath_callback), NULL);
-    gtk_box_pack_start(GTK_BOX(hbox1), choosebutton, FALSE, FALSE, 5);
+    chooseButton = gtk_file_chooser_button_new (("Select a file"), GTK_FILE_CHOOSER_ACTION_OPEN);
+    gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (chooseButton), "/home/barti/CLionProjects");
+    g_signal_connect(chooseButton, "selection-changed", G_CALLBACK(getPath_callback), NULL);
+    gtk_box_pack_start(GTK_BOX(hbox1), chooseButton, FALSE, FALSE, 5);
 
-    combo_box = gtk_combo_box_text_new ();
-    const char *time[] = {"0.2", "0.5", "1", "1.5", "2"};
+    comboBox = gtk_combo_box_text_new ();
+    const char *time[] = {"0,1", "0,2", "0,5", "1", "1,5", "2"};
     for (int i = 0; i < G_N_ELEMENTS (time); i++){
-        gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), time[i]);
+        gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (comboBox), time[i]);
     }
-    gtk_combo_box_set_active (GTK_COMBO_BOX (combo_box), 0);
-    g_signal_connect (combo_box, "changed", G_CALLBACK (on_changed), NULL);
-    gtk_box_pack_start(GTK_BOX(hbox1), combo_box, FALSE, FALSE, 5);
+    gtk_combo_box_set_active (GTK_COMBO_BOX (comboBox), 0);
+    g_signal_connect (comboBox, "changed", G_CALLBACK (on_changed), NULL);
+    gtk_box_pack_start(GTK_BOX(hbox1), comboBox, FALSE, FALSE, 5);
 
-    button1 = gtk_button_new_with_label("Load settings and start");
-    g_signal_connect(button1, "clicked", G_CALLBACK(checkConditions), NULL);
-    gtk_box_pack_start(GTK_BOX(hbox1), button1, FALSE, FALSE, 5);
+    startButton = gtk_button_new_with_label("Start");
+    g_signal_connect(startButton, "clicked", G_CALLBACK(checkConditions), NULL);
+    gtk_box_pack_start(GTK_BOX(hbox1), startButton, FALSE, FALSE, 5);
 
-    drawing_area = gtk_drawing_area_new ();
-    gtk_widget_set_size_request (drawing_area, 700, 600);
-    g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), NULL);
-    gtk_box_pack_start(GTK_BOX(vbox), drawing_area, FALSE, FALSE, 5);
+    drawingArea = gtk_drawing_area_new();
+    gtk_box_pack_start(GTK_BOX(vbox), drawingArea, TRUE, TRUE, 5);
+    g_signal_connect(drawingArea, "draw", G_CALLBACK(on_draw_event), NULL);
 
     gtk_widget_show_all (window);
 }
@@ -115,10 +160,11 @@ int main (int argc, char **argv){
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new ("pl.barti132", G_APPLICATION_FLAGS_NONE);
     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
     status = g_application_run (G_APPLICATION (app), argc, argv);
     g_object_unref (app);
 
+    freeMap();
     return status;
 }
